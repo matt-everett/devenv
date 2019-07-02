2de2893359a89bb577010d2d50e3ffad71adc57b
Matt Everett <matt.everett@mediakind.com>
2019-07-02 21:56:33 +0100

    Use virtualenv directly instead of the pyenv plugin.

--- docker/install/pyenv.sh
+++ docker/install/pyenv.sh
@@ -3,39 +3,15 @@
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 echo '' >> ${HOME}/.bashrc
-echo 'source ${HOME}/.pyenvrc' >> ${HOME}/.bashrc
+#echo 'source ${HOME}/.pyenvrc' >> ${HOME}/.bashrc
 source ${HOME}/.pyenvrc
 
 function install_python_version () {
     pyenv install $1
-            
-    pyenv local $1
-    # Upgrade pip to latest to avoid warnings
-    pip install --upgrade pip
-    pyenv local --unset
-
     echo "Installed python version $1"
 }
 
 declare -A versions
-
-while IFS=$'\t' read -r name version src; do
-    if [ ! ${versions[${version}]+_} ]; then
-        versions[${version}]="created"
-        install_python_version ${version}
-    fi
-    
-    pyenv virtualenv ${version} ${name}
-    pyenv activate ${name}
-
-    # Upgrade pip to latest to avoid warnings
-    pip install --upgrade pip
-    # vscode needs pylint
-    pip install --upgrade pylint
-    
-    pyenv deactivate
-    echo "Created/initialised virtualenv for ${name} with version ${version}."
-done < <(jq -r '.microservices[] | [.name,.pythonVersion,.src] | @tsv' ${HOME}/install/config.json)
 
 globalVersion=$(jq -r '[.globalPythonVersion] | @tsv' ${HOME}/install/config.json)
 if [ ! -z "${globalVersion}" ]; then
@@ -45,6 +21,39 @@ if [ ! -z "${globalVersion}" ]; then
     fi
 
     pyenv global ${globalVersion}
-    pip install tox
+    pip install --upgrade tox
+    pip install --upgrade virtualenv
+    pip install --upgrade pip
     echo "Python version ${globalVersion} used as global python."
 fi
+
+mkdir -p ${HOME}/.virtualenvs
+while IFS=$'\t' read -r name version src; do
+    if [ ! ${versions[${version}]+_} ]; then
+        versions[${version}]="created"
+        install_python_version ${version}
+    fi
+    
+    #pyenv virtualenv ${version} ${name}
+    #pyenv activate ${name}
+
+    pyenv local ${version}
+
+    # Upgrade pip to latest to avoid warnings
+    #pip install --upgrade pip
+    # vscode needs pylint
+    #pip install --upgrade pylint
+    pythonPath=$(pyenv which python)
+    echo "pythonPath: ${pythonPath}"
+
+    pyenv local --unset
+
+    virtualenv --python ${pythonPath} ${HOME}/.virtualenvs/${name}
+    source ${HOME}/.virtualenvs/${name}/bin/activate
+    pip install --upgrade pip
+    pip install --upgrade pylint
+    deactivate
+
+    #pyenv deactivate
+    echo "Created/initialised virtualenv for ${name} with version ${version}."
+done < <(jq -r '.microservices[] | [.name,.pythonVersion,.src] | @tsv' ${HOME}/install/config.json)
